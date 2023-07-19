bb490bb8a523a89845448cc3b24c15142ff67e53
carlosFuenmayor <cfuenmayor04@gmail.com>
2023-07-19 17:57:53 -0300

    Proyecto entrega n1 Carlos Eduardo Fuenmayor Villarreal

---
+++ page-js/index.js
@@ -0,0 +1,12 @@
+let arreglo= [24 , 54 , 25 , 577 ];
+console.log(arreglo);
+console.log(arreglo.length);
+
+arreglo.push("carlos");
+console.log(arreglo);
+arreglo.pop();
+console.log(arreglo);
+
+arreglo.unshift(542);
+console.log(arreglo);
+console.log(arreglo.entries())
